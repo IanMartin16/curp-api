b8b1e775f0bd29de24492da258d2b0e32758983e
ianmartin16 <imartindm20@gmail.com>
2025-12-03 17:14:40 -0600

    Add logs and stats

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import curpRouter from './routes/curp.routes';
+import { logsMiddleware } from "./middlewares/logs.middleware";
+import adminRouter from "./routes/admin.routes";
 
 dotenv.config();
 
@@ -10,6 +12,8 @@ const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
+app.use(logsMiddleware);
+app.use("/api/admin", adminRouter);
 
 // Registrar rutas
 app.use('/api/curp', curpRouter);
--- src/routes/curp.routes.ts
+++ src/routes/curp.routes.ts
@@ -1,9 +1,10 @@
-import { Router } from 'express';
-import { validateCurpController } from '../services/curp.service';
+import { Router } from "express";
+import { validateCurpController } from "../services/curp.service";
+import { apiKeyMiddleware } from "../middlewares/apikey.middleware";
 
 const router = Router();
 
-// POST /api/curp/validate
-router.post('/validate', validateCurpController);
+// Aplica el middleware SOLO a esta ruta (o a todas las del router)
+router.post("/validate", apiKeyMiddleware, validateCurpController);
 
 export default router;
