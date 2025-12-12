f42736e816282d7f56aa53f2d634e0237b50f902
ianmartin16 <imartindm20@gmail.com>
2025-12-12 07:12:55 -0600

    Updated CURP-Route

--- src/routes/curp.routes.ts
+++ src/routes/curp.routes.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import { validateCurpController } from "../services/curp.service";
-import { apiKeyMiddleware } from "../middlewares/apiKey.middleware";
 
 const router = Router();
 
 // Aplica el middleware SOLO a esta ruta (o a todas las del router)
-router.post("/validate", apiKeyMiddleware, validateCurpController);
+router.post("/validate", validateCurpController);
 
 export default router;
